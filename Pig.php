a97815bdc144d827544fac6fef41a95afc665d85
Adam Young <adamyoungy678@gmail.com>
2023-03-29 11:57:54 +0100

    added getters and setters for each pig method

--- Pig.php
+++ Pig.php
@@ -11,6 +11,56 @@ class Pig
     public string $colour;
 
     // Methods
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return float
+     */
+    public function getWeight(): float
+    {
+        return $this->weight;
+    }
+
+    /**
+     * @param float $weight
+     */
+    public function setWeight(float $weight): void
+    {
+        $this->weight = $weight;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColour(): string
+    {
+        return $this->colour;
+    }
+
+    /**
+     * @param string $colour
+     */
+    public function setColour(string $colour): void
+    {
+        $this->colour = $colour;
+    }
+
+
+
     public function speak(): string
     {
         return 'Oink';
@@ -24,6 +74,7 @@ class Pig
         return 'Mmmm. I love tasty ' . $food;
     }
 }
+
 
 // -> is called the object operator or object accessor.
 
