b5c8a9fe9bf9d491728246ee2bf59f23ff00d60d
Adam Young <adamyoungy678@gmail.com>
2023-03-29 15:19:56 +0100

    copied pig class into new cow class and changed scientific name

---
+++ Cow.php
@@ -0,0 +1,92 @@
+<?php
+
+// A class is the specification/description/blueprint of objects.
+// It's a kind of data type.
+
+class Cow
+{
+    // Properties
+    const Owner = "IO Academy";
+    private string $name;
+    public float $weight;
+    public string $colour;
+    private string $scientificName = 'Bos';
+
+    /**
+     * @return string
+     */
+    public function getScientificName(): string
+    {
+        return $this->scientificName;
+    }
+
+    // Methods
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return float
+     */
+    public function getWeight(): float
+    {
+        return $this->weight;
+    }
+
+    /**
+     * @param float $weight
+     */
+    public function setWeight(float $weight): void
+    {
+        $this->weight = $weight;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColour(): string
+    {
+        return $this->colour;
+    }
+
+    /**
+     * @param string $colour
+     */
+    public function setColour(string $colour): void
+    {
+        $this->colour = $colour;
+    }
+
+
+
+    public function speak(): string
+    {
+        return 'Oink';
+    }
+
+    public function wagTail(): string
+    {
+        return 'Wag, wag!';
+    }
+    public function eat(string $food): string {
+        return 'Mmmm. I love tasty ' . $food;
+    }
+}
+
+
+// -> is called the object operator or object accessor.
+
+
+
